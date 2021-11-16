dac083f17a92d5aeba06e3300ec6e316ea8104c0
Dhayvison Braga <dhayvison.braga@hostweb.com.br>
2021-11-15 21:51:02 -0300

    chore: add barcode page route

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,3 +1,4 @@
+import 'modules/barcode_scanner/barcode_scanner_page.dart';
 import 'modules/home/home_page.dart';
 import 'modules/login/login_page.dart';
 import 'modules/splash/splash_page.dart';
@@ -6,10 +7,12 @@ class AppRoutes {
   static const splashPage = '/splash';
   static const homePage = '/home';
   static const loginPage = '/login';
+  static const barcodeScannerPage = '/barcode_scanner';
 
   static final routes = {
     splashPage: (context) => const SplashPage(),
     homePage: (context) => const HomePage(),
     loginPage: (context) => const LoginPage(),
+    barcodeScannerPage: (context) => const BarcodeScannerPage(),
   };
 }
