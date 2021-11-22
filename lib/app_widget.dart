96ebbb799d36fc687a98618e82fd8a9ef663cb3e
Dhayvison <dhayvisonbraga@hotmail.com>
2021-11-22 20:19:10 -0300

    chore: test

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -11,8 +11,8 @@ class AppWidget extends StatelessWidget {
       title: 'Pay Flow',
       initialRoute: AppRoutes.splashPage,
       routes: AppRoutes.routes,
-      theme: ThemeData(
-          primarySwatch: Colors.orange, primaryColor: AppColors.primary),
+      theme:
+          ThemeData(primarySwatch: Colors.orange, primaryColor: AppColors.body),
     );
   }
 }
